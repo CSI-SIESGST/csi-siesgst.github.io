40662e609f9ac9f5061a864c782162f8a409b16b
ac030540 <ac030540@gmail.com>
2020-01-26 19:04:57 +0530

    Added animation of border width and added cloudinary links

--- js/index.js
+++ js/index.js
@@ -2,22 +2,32 @@ var section = document.querySelector('.numbers');
 var hasEntered = false;
 
 window.addEventListener('scroll', (e) => {
-    var shouldAnimate = (window.scrollY + window.innerHeight) >= section.offsetTop;
+  var shouldAnimate = (window.scrollY + window.innerHeight) >= section.offsetTop;
 
-    if (shouldAnimate && !hasEntered) {
+  if (shouldAnimate && !hasEntered) {
     hasEntered = true;
 
     $('.value').each(function () {
-        $(this).prop('Counter',0).animate({
+      $(this).prop('Counter',0).animate({
         Counter: $(this).text()
-        }, {
+      },
+      {
         duration: 4000,
         easing: 'swing',
         step: function (now) {
             $(this).text(Math.ceil(now));
         }
-        });
+      });
     });
 
+    $('.circle').each(function () {
+      $(this).css({"border": "0px solid #1D75FC"}).animate({
+        'borderWidth': '10px'
+      },
+      {
+        duration: 4000,
+        easing: 'swing'
+      });
+    });
   }
 });

2cca7cde4fc51a550e5b20f5c59c5f305a876252
Suriya Prasath.S <48484191+SP-2605@users.noreply.github.com>
2020-02-02 06:31:06 +0530

    CSI_SIESGST Updated [yet not responsive]

--- js/index.js
+++ js/index.js
@@ -22,7 +22,7 @@ window.addEventListener('scroll', (e) => {
 
     $('.circle').each(function () {
       $(this).css({"border": "0px solid #1D75FC"}).animate({
-        'borderWidth': '10px'
+        'borderWidth': '1.5rem'
       },
       {
         duration: 4000,
